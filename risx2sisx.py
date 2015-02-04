aba9b4100f2fa8efce18a4537b92e9c34206afb1
Brendan Long <self@brendanlong.com>
2015-02-04 12:23:28 -0600

    Handle arguments.

--- risx2sisx.py
+++ risx2sisx.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+import argparse
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("representation_index", help="The representation "
+		"index to convert (generally a .sidx file).")
+	parser.add_argument("--template", "-t", help="Template for segment index "
+		"files. {n} will be replaced with the segment number.",
+		default="segment-{n}.sidx")
+
+	args = parser.parse_args()
